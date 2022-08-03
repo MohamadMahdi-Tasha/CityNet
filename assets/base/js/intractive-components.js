b35af43179a9bad99979ca639ba2e30f19fbe06d
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-08-03 14:02:30 +0430

    added jses

--- assets/base/js/intractive-components.js
+++ assets/base/js/intractive-components.js
@@ -1,4 +1,26 @@
+//Variables
 const popularCitysBtn = document.querySelectorAll('.popular-citys-btn');
+const switchBtn = document.querySelectorAll('.switch-btn');
+const plusBtns = document.querySelectorAll('.plus-btn');
+const minusBtns = document.querySelectorAll('.minus-btn');
+const dateComponent = document.querySelectorAll('.intractive-input-component.date');
+const passangerComponent = document.querySelectorAll('.intractive-input-component.passenger')
+const oneWayPassengerDropdownItems = document.querySelectorAll('.one-way-passenger-dropdown > .dropdown-menu button');
+const minusAndPlusButtons = document.querySelectorAll('.minus-and-plus-btn');
+
+// A Function That Sets Value Of Passengers Interactive Components, Input To Total Number Of adultNumber, kidNumber And newBornNumber
+// And Value Of Attritube Of data-value From DropDown Init.
+function setValueOfPassangerInput() {
+    const adultNumber = Number(document.querySelector('.adult-number').textContent);
+    const kidNumber = Number(document.querySelector('.kid-number').textContent);
+    const newBornNumber = Number(document.querySelector('.new-born-number').textContent);
+    const dropdown = document.querySelector('.one-way-passenger-dropdown');
+    const passengerNumber = document.querySelector('.passenger-number');
+    const passengerType = document.querySelector('.passenger-type');
+
+    passengerNumber.textContent = adultNumber + kidNumber + newBornNumber;
+    passengerType.textContent = dropdown.getAttribute('data-value');
+}
 
 popularCitysBtn.forEach(item => {
     item.addEventListener('click', () => {
@@ -61,3 +83,76 @@ popularCitysBtn.forEach(item => {
         component.classList.add('focused');
     })
 })
+
+dateComponent.forEach(item => {
+    item.addEventListener('click', () => item.lastElementChild.focus())
+    item.lastElementChild.addEventListener('focus', () => item.classList.add('focused'))
+    item.lastElementChild.addEventListener('blur', () => {
+        if (item.lastElementChild.value !== "") {item.classList.add('focused')}
+    })
+})
+
+// Adding Event Listener Of Click To Each Switch Buttons That Gets Clone Of Item Of 'selected-location' In Component Then Replaces The Orginals
+// With Clones And Adds Class Name Of animation To Switch Button And On Event Of 'animationend' (Its When Animation Ends) Removes It
+switchBtn.forEach(item => {
+    item.addEventListener('click', () => {
+        const rightSideComponentContentToChange = document.querySelector('.top-side-flight > .intractive-input-component:first-of-type .selected-location');
+        const leftSideComponentContentToChange = document.querySelector('.top-side-flight > .intractive-input-component:last-of-type .selected-location');
+        const cloneOfRightSide = rightSideComponentContentToChange.cloneNode(true);
+        const cloneOfLeftSide = leftSideComponentContentToChange.cloneNode(true);
+
+        rightSideComponentContentToChange.replaceWith(cloneOfLeftSide)
+        leftSideComponentContentToChange.replaceWith(cloneOfRightSide)
+
+        item.classList.add('animation');
+        item.addEventListener('animationend', () => {item.classList.remove('animation')})
+    })
+})
+
+// Adding Event Listener Event On Each Plus Button That Listens To Click And Checks If Number Is Not 9 Then Adds 1 To It
+plusBtns.forEach(item => {
+    item.addEventListener('click', () => {
+        const number = item.nextElementSibling;
+        let realNumber = Number(number.textContent);
+
+        if (realNumber !==  9) {realNumber ++;}
+        number.textContent = realNumber;
+    })
+})
+
+// Adding Event Listener Event On Each Plus Button That Listens To Click And Checks If Number Is Not 9 Then Reduces 1 From It
+minusBtns.forEach(item => {
+    item.addEventListener('click', () => {
+        const number = item.previousElementSibling;
+        let realNumber = Number(number.textContent);
+
+        if (realNumber !==  0) {realNumber --;}
+        number.textContent = realNumber;
+    })
+})
+
+passangerComponent.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('show')
+        item.nextElementSibling.classList.toggle('show')
+    })
+})
+
+// Adding Event Listener On  Each Dropdown Item In Passenger Component And Listens To Click Then Checks If There Is Dropdown Item With
+// Attribute Of data-active="true" Then Removes Attribute From It And Sets data-Value Of Dropdown To Clicked Items TextContent And
+// Sets Text Content OF Input In Component To Clicked Items TextContent Again And Sets Attritube Of Clicked item To 'true' (data-active)
+// And Calls setValueOfPassangerInput Function
+oneWayPassengerDropdownItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const activeButtons = document.querySelectorAll('.one-way-passenger-dropdown .dropdown-item[data-active="true"]');
+        activeButtons.forEach(item => item.removeAttribute('data-active'));
+
+        item.parentElement.parentElement.parentElement.setAttribute('data-value', item.textContent);
+        item.parentElement.parentElement.previousElementSibling.firstElementChild.textContent = item.textContent
+        item.setAttribute('data-active', 'true');
+        setValueOfPassangerInput()
+    })
+})
+
+//  Adding Event Listener On Click Of Each Minus And Plus Button That Calls setValueOfPassangerInput Function
+minusAndPlusButtons.forEach(item => item.addEventListener('click', () => setValueOfPassangerInput()))
