9abc6ae5d80532aeb8c3691f37c810b886bb93b1
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-08-06 10:27:59 +0430

    added js of top side buttons

--- assets/base/js/inner-pages.js
+++ assets/base/js/inner-pages.js
@@ -1,5 +1,6 @@
 // Variables
 const homeFlightInnerPageToggler = document.querySelectorAll('.home-flight-inner-page-toggler');
+const topSidePageButtons = document.querySelectorAll('.top-side-page-buttons');
 
 // Adding Event Listener On Each 'homeFlightInnerPageToggler's That Listens To Click And Reads Attribute Of 'data-target' Of It
 // And Then Gets It By 'document.getElementById' Method. Then Replaces Class Of 'current' With 'd-none'  On Element That Have Class Of
@@ -10,5 +11,22 @@ homeFlightInnerPageToggler.forEach(item => {
         const target = document.getElementById(targetId);
         document.querySelector('.home-flight-inner-page.current').classList.replace('current', 'd-none');
         target.classList.replace('d-none', 'current');
+    })
+})
+
+// Adding Event Listener On Each Top Side Buttons That Listens To Click And Removes Active Class Name From Top Side Buttons That Have It
+// Then Replaces 'current' ClassName With 'd-none' In Current Bottom Page That Is Showing And Replaces 'd-none' To 'current' In Target Of
+// Clicked Button Then Adds Active To Clicked Button
+topSidePageButtons.forEach(item => {
+    item.addEventListener('click', () => {
+        const targetId = item.getAttribute('data-target');
+        const target = document.getElementById(targetId);
+        const activeTopSideButton = document.querySelector('.top-side-page-buttons.active');
+        const bottomPageCurrent = document.querySelector('.bottom-page.current');
+
+        activeTopSideButton.classList.remove('active');
+        bottomPageCurrent.classList.replace('current', 'd-none');
+        target.classList.replace('d-none', 'current');
+        item.classList.add('active');
     })
 })
