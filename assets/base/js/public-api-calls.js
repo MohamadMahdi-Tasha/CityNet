5de02a40fb8680903f39d3dcf7bd3ed2320aee00
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-07 16:00:43 +0430

    idk

--- assets/base/js/public-api-calls.js
+++ assets/base/js/public-api-calls.js
@@ -1,5 +1,6 @@
 //Variables
-const settings = {"method": "GET", "headers": {"Accept": "application/json", "Content-Type": "application/json"},}
+const getReqSettings = {"method": "GET", "headers": {"Accept": "application/json", "Content-Type": "application/json"},}
+const postReqSettings = {"method": "POST", "headers": {"Accept": "application/json", "Content-Type": "application/json"},}
 const loggedInButtonHeader = document.getElementById('logged-in-button-header');
 
 // Adding Event Listner On Logged In Button In Header That Opens It Drop Down
@@ -8,7 +9,7 @@ loggedInButtonHeader.addEventListener('click', () => loggedInButtonHeader.nextEl
 // Fetching Login Api That Checks If Massage Of Returned Data Is "لم یتم العثور علی مستخدم برقم الهاتف هذا"
 // Which Means "User With This Number Not Found". If It Is Adds Class Of 'not-logged-in' To Html ELement
 // Other Wise Adds 'logged-in' Class Name To It And Closes Loader Modal Which Shows At First And gets Console.log Of errors For Debuging Purposes
-fetch('https://www.newcash.me/api/v1/airfare-web/login', settings)
+fetch('https://www.newcash.me/api/v1/airfare-web/login', getReqSettings)
     .then(data => data.json())
     .then(data => {
         const massage = data.data.msg;
