f503e07216c2dc0eab33b31b9b010c247acf743e
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-10 14:21:25 +0430

    added login code section

--- assets/base/js/public-api-calls.js
+++ assets/base/js/public-api-calls.js
@@ -1,6 +1,9 @@
 // Variables
 const modalLoginMobilePage = document.getElementById('modal-login-mobile-page');
 const verifyModal = document.getElementById('verify-modal');
+const verifyForm = document.getElementById('verify-form');
+const numberInput = modalLoginMobilePage.firstElementChild.firstElementChild.firstElementChild.lastElementChild;
+const htmlElement = document.querySelector('html');
 
 modalLoginMobilePage.addEventListener('submit', (event) => {
   // Preventing From Defualt action Of Form
@@ -9,8 +12,7 @@ modalLoginMobilePage.addEventListener('submit', (event) => {
   // Variables
   const spinner = document.createElement('span');
   const myHeaders = new Headers();
-  const numberInput = modalLoginMobilePage.firstElementChild.firstElementChild.firstElementChild.lastElementChild;
-  const phoneNumberInputValue = numberInput.value.trim();
+  const phoneNumberInputValue = numberInput.value;
   const dataToSend = JSON.stringify({"mobile": phoneNumberInputValue});
   const requestOptions = {
     method: 'POST',
@@ -19,10 +21,10 @@ modalLoginMobilePage.addEventListener('submit', (event) => {
     redirect: 'follow'
   };
 
-  // Showing Spinner In Submit Button Of Form
-  modalLoginMobilePage.lastElementChild.textContent = '';
-  spinner.className = 'spinner-border text-white';
-  modalLoginMobilePage.lastElementChild.appendChild(spinner)
+    // Showing Spinner In Submit Button Of Form
+    spinner.className = 'spinner-border text-white';
+    modalLoginMobilePage.lastElementChild.textContent = '';
+    modalLoginMobilePage.lastElementChild.appendChild(spinner)
 
   // Setting Headers
   myHeaders.append("Accept", "application/json");
@@ -52,4 +54,54 @@ modalLoginMobilePage.addEventListener('submit', (event) => {
         }
       })
       .catch(error => console.log('error', error));
+})
+
+verifyForm.addEventListener('submit', (event) => {
+    // Preventing From Defualt action Of Form
+    event.preventDefault();
+
+    // variables
+    const myHeaders = new Headers();
+    const dataToSend = JSON.stringify({"mobile": numberInput.value, "verify_code": verifyCodeInput.value});
+    const requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: dataToSend,
+        redirect: 'follow'
+    };
+    const spinner = document.createElement('span');
+
+    // Showing Spinner In Submit Button Of Form
+    spinner.className = 'spinner-border text-white';
+    verifyForm.lastElementChild.textContent = '';
+    verifyForm.lastElementChild.appendChild(spinner)
+
+    // Setting Headers
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("debug", "true");
+    myHeaders.append("Content-Type", "application/json");
+
+    // Fetching Verify Api
+    fetch("https://www.newcash.me/api/v1/airfare-web/verify", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            const returnedToken = result.data.token;
+
+            verifyForm.lastElementChild.textContent = 'ورود';
+            spinner.remove();
+
+            if (returnedToken === null) {
+                handleError(
+                    verifyCodeInput.parentElement,
+                    verifyCodeInput.parentElement.nextElementSibling,
+                    verifyCodeInput.parentElement.nextElementSibling.firstElementChild,
+                    'کد اشتباه وارد شده'
+                )
+            } else {
+                handleSuccses(verifyCodeInput.parentElement, verifyCodeInput.parentElement.nextElementSibling)
+                verifyModal.setAttribute('data-opened', 'false');
+                htmlElement.setAttribute('logged-in', 'true');
+            }
+        })
+        .catch(error => console.log('error', error));
 })
