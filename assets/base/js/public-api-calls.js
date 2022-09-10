efe1d929235072b4f40d472767b55b976d1cf434
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-09-10 10:50:14 +0430

    adde lines of comment

--- assets/base/js/public-api-calls.js
+++ assets/base/js/public-api-calls.js
@@ -1,7 +1,7 @@
 // Variables
 const loggedInButtonHeader = document.getElementById('logged-in-button-header');
 const htmlElement = document.querySelector('html');
-const data = JSON.stringify({"mobile": "9109181033"});
+const dataToSend = JSON.stringify({"mobile": "9109181033"});
 const xhr = new XMLHttpRequest();
 
 // Adding Event Listener On Logged In Button In Header That Opens It Drop Down
@@ -11,8 +11,10 @@ loggedInButtonHeader.addEventListener('click', () => loggedInButtonHeader.nextEl
 // Status Is True Then It Adds Class Of 'logged-in' To Html Element Of Page.
 const setLoggedInByStatus = (status1, status2) => {if (status1) {htmlElement.classList.add('not-logged-in');} else if (status2) {htmlElement.classList.add('logged-in');}};
 
-
+// Setting Credential Of Our XH Request To True
 xhr.withCredentials = true;
+
+// Adding Event Listener To When Our XHR Is Ready . Then Calls 'setLoggedInByStatus' Function In given Statements
 xhr.addEventListener("readystatechange", function() {
   if(this.readyState === 4) {
     const thereIsNoUserWithThisNumber = "لم یتم العثور علی مستخدم برقم الهاتف هذا";
@@ -22,9 +24,11 @@ xhr.addEventListener("readystatechange", function() {
   }
 });
 
+// Some Properties For XHR
 xhr.open("GET", "https://www.newcash.me/api/v1/airfare-web/login");
 xhr.setRequestHeader("Accept", "application/json");
 xhr.setRequestHeader("debug", "true");
 xhr.setRequestHeader("Content-Type", "application/json");
 
-xhr.send(data);
+// Sending XH Request With Data To Send
+xhr.send(dataToSend);
