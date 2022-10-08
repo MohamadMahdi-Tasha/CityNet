fdea4df647cee29e89646ba25fda6d42fcd2f71e
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-10-08 17:57:35 +0330

    added line of comment

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -1,8 +1,5 @@
 // Variables
 const dropDownToggler = document.querySelectorAll('.my-drop-down-holder > .my-drop-down-toggler');
 
-dropDownToggler.forEach(item => {
-    item.addEventListener('click', () => {
-        item.parentElement.toggleAttribute('data-opened');
-    })
-})
+// Adding Event Listener On Each Drop Down Toggler That Toggles Attribute Of 'data-opened' To Clicked Items Parent Element
+dropDownToggler.forEach(item => item.addEventListener('click', () => item.parentElement.toggleAttribute('data-opened')))
