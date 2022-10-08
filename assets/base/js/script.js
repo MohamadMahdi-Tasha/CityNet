49329f2f9678e84d666f1bdab9191f0b445bd943
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-10-08 18:17:30 +0330

    added intractions to currency change buttons in header

--- assets/base/js/script.js
+++ assets/base/js/script.js
@@ -1,6 +1,8 @@
 // Variables
 const dropDownToggler = document.querySelectorAll('.my-drop-down-holder > .my-drop-down-toggler');
 const togglers = document.querySelectorAll('.toggler');
+const currencyChanger = document.querySelectorAll('.currency-changer');
+const htmlElement = document.querySelector('html');
 
 // Adding Event Listener On Each Drop Down Toggler That Toggles Attribute Of 'data-opened' To Clicked Items Parent Element
 dropDownToggler.forEach(item => item.addEventListener('click', () => item.parentElement.toggleAttribute('data-opened')))
@@ -13,5 +15,14 @@ togglers.forEach(item => {
         const targetElement = document.getElementById(target);
 
         targetElement.toggleAttribute('data-opened');
+    })
+})
+
+// Adding Event Listener On Each Item With Class Of 'currency-changer' That Listens To Click And Sets Attribute Of 'data-currency' In Html Element
+// To Value Of 'data-item-currency' Attribute Of Clicked Item
+currencyChanger.forEach(item => {
+    item.addEventListener('click', () => {
+        const itemsCurrency  = item.getAttribute('data-item-currency');
+        htmlElement.setAttribute('data-currency', itemsCurrency)
     })
 })
