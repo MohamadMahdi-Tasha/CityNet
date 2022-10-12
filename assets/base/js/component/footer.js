cb41b36fc135490acf64b9fe49c5ea39fe62c3df
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-10-12 12:44:16 +0330

    added styles of footer

--- assets/base/js/component/footer.js
+++ assets/base/js/component/footer.js
@@ -3,29 +3,31 @@ class Footer extends HTMLElement {
     constructor() {super();}
     connectedCallback() {
         this.innerHTML = `
-            <footer>
-                <div>
-                    <div>
-                        <img src="assets/base/img/footer/img-logo-typo.png" alt="سیتی نت">
-                        <ul>
-                            <li><a href="#">صفحه اصلی</a></li>
-                            <li><a href="#">تماس با ما</a></li>
-                            <li><a href="#">درباره ما</a></li>
-                            <li><a href="#">قوانین و مقررات</a></li>
-                        </ul>
+            <footer class="d-flex justify-content-center">
+                <div class="container-xxl px-lg-5 py-lg-3 px-2 py-2">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <div>
+                            <img width="300px" src="assets/base/img/footer/img-logo-typo.png" alt="سیتی نت" class="mb-4">
+                            <ul class="d-flex list-unstyled gap-3 p-0 m-0">
+                                <li><a class="text-grey-darken1 transition-3 font-small text-hover-pink" href="#">صفحه اصلی</a></li>
+                                <li><a class="text-grey-darken1 transition-3 font-small text-hover-pink" href="#">تماس با ما</a></li>
+                                <li><a class="text-grey-darken1 transition-3 font-small text-hover-pink" href="#">درباره ما</a></li>
+                                <li><a class="text-grey-darken1 transition-3 font-small text-hover-pink" href="#">قوانین و مقررات</a></li>
+                            </ul>
+                        </div>
+                        <div class="d-flex gap-3">
+                            <div class="rounded-3 bg-white p-3"><img width="50px" src="assets/base/img/footer/img-nerkhebilit.png" alt="نرخ بیلیط"></div>
+                            <div class="rounded-3 bg-white p-3"><img width="50px" src="assets/base/img/footer/img-havapeymaei-keshvari.png" alt="هواپیمایی کشوری"></div>
+                            <div class="rounded-3 bg-white p-3"><img width="50px" src="assets/base/img/footer/img-hoghoghe-mosafer.png" alt="حقوق مسافر"></div>
+                        </div>
                     </div>
-                    <div>
-                        <div><img src="assets/base/img/footer/img-nerkhbilit.png" alt="نرخ بیلیط"></div>
-                        <div><img src="assets/base/img/footer/img-havapeymaei-keshvari.png" alt="هواپیمایی کشوری"></div>
-                        <div><img src="assets/base/img/footer/img-hoghoghe-mosafer.png" alt="حقوق مسافر"></div>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <h6  class="text-grey-darken1 font-small">تمامی حقوق متعلق به سیتی نت میباشد.</h6>
+                        <h6  class="text-grey-darken1 font-small">
+                         <a class="transition-3 text-grey-darken1 text-hover-pink" href="#">طراحی سایت آژانس مسافرتی</a>
+                         توسط سیتی نت
+                        </h6>
                     </div>
-                </div>
-                <div>
-                    <h6>تمامی حقوق متعلق به سیتی نت میباشد.</h6>
-                    <h6>
-                     <a href="#">طراحی سایت آژانس مسافرتی</a>
-                     توسط سیتی نت
-                    </h6>
                 </div>
             </footer>
         `
