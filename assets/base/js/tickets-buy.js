985eff875848258931389643fa651ea352055d19
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-10-21 12:43:43 +0330

    added scripts of left side top in last commit

--- assets/base/js/tickets-buy.js
+++ assets/base/js/tickets-buy.js
@@ -1,5 +1,6 @@
 // Variables
 const buyTicketDropDownButton = document.querySelectorAll('.buy-ticket-drop-down-button');
+const intractiveButtonsBtnTicketBuy = document.querySelectorAll('.intractive-buttons-btn.ticket-buy');
 
 buyTicketDropDownButton.forEach(item => {
     item.addEventListener('click', () => {
@@ -11,5 +12,19 @@ buyTicketDropDownButton.forEach(item => {
         parentOfGrandParentOfClickedItem.previousElementSibling.firstElementChild.nextElementSibling.textContent = item.textContent
         buyTicketDropDownButtonActive.classList.remove('active');
         item.classList.add('active');
+    })
+})
+
+intractiveButtonsBtnTicketBuy.forEach(item => {
+    item.addEventListener('click', () => {
+        const target = item.getAttribute('data-target');
+        const targetElement = document.getElementById(target);
+        const activeInnerTab = document.querySelector('.buy-ticket-left-side-inner-tabs[active]');
+        const activeButton = document.querySelector('.intractive-buttons-btn.ticket-buy[active]');
+
+        activeInnerTab.removeAttribute('active');
+        activeButton.removeAttribute('active');
+        item.setAttribute('active', 'true');
+        targetElement.setAttribute('active', 'true');
     })
 })
