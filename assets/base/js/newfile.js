6ba205c8f5340b539e704ad1e2b3f4828c755e2b
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-10-30 17:36:54 +0330

    seprated written scripts

---
+++ assets/base/js/newfile.js
@@ -0,0 +1,22 @@
+const myCollapseToggler = document.querySelectorAll('.my-collapse-toggler');
+const ticketPageAsideMobileTopSideToggle = document.querySelector('.ticket-page-aside-mobile-top-side-toggle');
+const ticketPageAside = document.querySelector('.ticket-page-aside');
+const ticketComponentBottomAccardionToggler = document.querySelectorAll('.ticket-component-bottom-accardion-toggler');
+
+ticketPageAsideMobileTopSideToggle.addEventListener('mousedown', () => ticketPageAside.classList.toggle('top'))
+
+window.onscroll = () => (window.scrollY !== 0) ? ticketPageAside.classList.add('scrolled') : ticketPageAside.classList.remove('scrolled');
+
+ticketComponentBottomAccardionToggler.forEach(item => {
+    item.addEventListener('click', () => {
+        const target = item.getAttribute('data-accardion-target');
+        const targetElement = document.getElementById(target);
+        const openedAccardionItem = document.querySelector('.ticket-component-bottom-accardion[data-opened]');
+        const openedAccardionToggler = document.querySelector('.ticket-component-bottom-accardion-toggler[data-opened]');
+
+        item.toggleAttribute('data-opened');
+        targetElement.toggleAttribute('data-opened');
+        openedAccardionItem.removeAttribute('data-opened');
+        openedAccardionToggler.removeAttribute('data-opened');
+    })
+})
