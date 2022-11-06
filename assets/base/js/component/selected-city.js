9e6ba6c7bc947f5caca91da88db957b6965615fa
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-11-06 16:49:27 +0330

    added some other styles to last commit

--- assets/base/js/component/selected-city.js
+++ assets/base/js/component/selected-city.js
@@ -3,8 +3,8 @@ class selectedCity extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="d-flex align-items-center">
-                <div class="d-flex align-items-center ms-2 bg-pink p-1 text-white rounded-3">
-                    <i class="bi ms-2 bi-${(this.getAttribute('primary') !== null) ? 'house' : 'plane rotate-270'}"></i>
+                <div class="d-flex align-items-center ms-2 p-1 text-white rounded-3 bg-${(this.getAttribute('primary') !== null) ? 'pink' : 'info-base'}">
+                    <i class="bi ms-2 bi-${(this.getAttribute('primary') !== null) ? 'house' : 'airplane rotate-270'}"></i>
                     <h6 class="mb-0 font-small">${this.getAttribute('brand')}</h6>
                 </div>
                 <h6 class="mb-0 font-small text-grey-darken3">${this.getAttribute('title')}</h6>
