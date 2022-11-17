9ebf483ce4d898aaa3489849fa98d46e0f138933
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-11-17 22:23:56 +0330

    madhouse

--- assets/base/js/pages/scripts/index.js
+++ assets/base/js/pages/scripts/index.js
@@ -11,9 +11,31 @@ searchForTicketButton.addEventListener('click', () => {
     const startDateIntractiveComponentsInnerButton = startDateIntractiveComponents.firstElementChild
 
     if (startCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') === 'null') {setErrorOnComponent(startCityIntractiveComponents, 'لطفا شهر مبدا را انتخواب کنید')}
-    if (startCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') !== 'null') {setSuccsesOnComponent(startCityIntractiveComponents)}
+    else if (startCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') !== 'null') {setSuccsesOnComponent(startCityIntractiveComponents)}
     if (endCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') === 'null') {setErrorOnComponent(endCityIntractiveComponents, 'لطفا شهر مقصد را انتخواب کنید')}
-    if (endCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') !== 'null') {setSuccsesOnComponent(endCityIntractiveComponents)}
+    else if (endCityIntractiveComponentsInnerButton.getAttribute('data-selected-city') !== 'null') {setSuccsesOnComponent(endCityIntractiveComponents)}
     if (startDateIntractiveComponentsInnerButton.getAttribute('data-date') === 'null') {setErrorOnComponent(startDateIntractiveComponents, 'لطفا تاریخ پرواز را مشخص کنید')}
-    if (startDateIntractiveComponentsInnerButton.getAttribute('data-date') !== 'null') {setSuccsesOnComponent(startDateIntractiveComponents)}
+    else if (startDateIntractiveComponentsInnerButton.getAttribute('data-date') !== 'null') {setSuccsesOnComponent(startDateIntractiveComponents)}
+
+    if (document.querySelectorAll('.intractive-component.errored').length === 0) {
+        const dataDate = startDateIntractiveComponentsInnerButton.getAttribute('data-date');
+        const adultCount = passangerCountIntractiveComponents.firstElementChild.firstElementChild.getAttribute('data-adult-count');
+        const childCount = passangerCountIntractiveComponents.firstElementChild.firstElementChild.getAttribute('data-child-count');
+        const infantCount = passangerCountIntractiveComponents.firstElementChild.firstElementChild.getAttribute('data-infant-count');
+        const startCity = startCityIntractiveComponentsInnerButton.getAttribute('data-selected-city');
+        const endCity = endCityIntractiveComponentsInnerButton.getAttribute('data-selected-city');
+        const newLinkElement = document.createElement('a');
+        const informationToSearchInTickets = {
+            date: dataDate,
+            adult_count: adultCount,
+            child_count: childCount,
+            infant_count: infantCount,
+            from: startCity,
+            to: endCity
+        }
+
+        localStorage.setItem('informationToSearchInTickets', JSON.stringify(informationToSearchInTickets));
+        newLinkElement.setAttribute('a', 'tickets.html');
+        newLinkElement.click()
+    }
 })
