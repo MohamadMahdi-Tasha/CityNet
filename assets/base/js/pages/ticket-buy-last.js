be2b24bf0d0b47433063538c62e3ee2d9e2ad16b
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-11-24 12:49:31 +0330

    yea

--- assets/base/js/pages/ticket-buy-last.js
+++ assets/base/js/pages/ticket-buy-last.js
@@ -7,4 +7,37 @@ const submitButton = document.getElementById('submit-btn');
 acceptRulesInput.addEventListener('click', () => (acceptRulesInput.check) ? submitButton.removeAttribute('disabled') : submitButton.toggleAttribute('disabled'))
 
 window.addEventListener('load', () => {
+    const flightDetailsHolder = document.getElementById('flight-details-holder');
+    const flightDetails = document.createElement('flight-details');
+    const selectedTicket = JSON.parse(localStorage.getItem('selected-ticket'));
+
+    // Setting Attributes Of Created 'flight-details' Element
+    flightDetails.setAttribute('type', 'details');
+    flightDetails.setAttribute('icon-src', selectedTicket.icon);
+    flightDetails.setAttribute('name', selectedTicket.name);
+    flightDetails.setAttribute('flight-number', selectedTicket.flightNumber);
+    flightDetails.setAttribute('mode', 'اکونومی');
+    flightDetails.setAttribute('start-time', selectedTicket.startTime);
+    flightDetails.setAttribute('end-time', selectedTicket.endTime);
+    flightDetails.setAttribute('start-location', `(${selectedTicket.startLocation}) ${persianNameFromAbbrevation(selectedTicket.startLocation)}`);
+    flightDetails.setAttribute('start-location-fa', persianNameFromAbbrevation(selectedTicket.startLocation));
+    flightDetails.setAttribute('start-location-en', englishNameFromAbrevation(selectedTicket.startLocation));
+    flightDetails.setAttribute('start-loaction-abbr', selectedTicket.startLocation);
+    flightDetails.setAttribute('end-location', `(${selectedTicket.endLocation}) ${persianNameFromAbbrevation(selectedTicket.endLocation)}`);
+    flightDetails.setAttribute('end-location-fa', persianNameFromAbbrevation(selectedTicket.endLocation));
+    flightDetails.setAttribute('end-location-en', englishNameFromAbrevation(selectedTicket.endLocation));
+    flightDetails.setAttribute('end-loaction-abbr', selectedTicket.endLocation);
+    flightDetails.setAttribute('start-location', selectedTicket.endTime);
+    flightDetails.setAttribute('price', selectedTicket.price);
+    flightDetails.setAttribute('route-duration-hour', getRouteDuration(selectedTicket.startTime, selectedTicket.endTime).hour);
+    flightDetails.setAttribute('route-duration-minute', getRouteDuration(selectedTicket.startTime, selectedTicket.endTime).minute);
+    flightDetails.setAttribute('start-date', selectedTicket.date);
+    flightDetails.setAttribute('start-date-en', new Date(selectedTicket.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }));
+    flightDetails.setAttribute('start-date-fa', `${new persianDate(new Date(selectedTicket.date)).format('YYYY')} ${new persianDate(new Date(selectedTicket.date)).format('MMMM')} ${new persianDate(new Date(selectedTicket.date)).format('DD')}`);
+    flightDetails.setAttribute('end-date', selectedTicket.date);
+    flightDetails.setAttribute('end-date-en', new Date(selectedTicket.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }));
+    flightDetails.setAttribute('end-date-fa', `${new persianDate(new Date(selectedTicket.date)).format('YYYY')} ${new persianDate(new Date(selectedTicket.date)).format('MMMM')} ${new persianDate(new Date(selectedTicket.date)).format('DD')}`);
+    flightDetails.setAttribute('plane-model', selectedTicket.planeModel);
+
+    flightDetailsHolder.appendChild(flightDetails)
 })
