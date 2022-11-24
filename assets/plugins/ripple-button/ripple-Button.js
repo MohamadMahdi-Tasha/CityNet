906a49e552c984c3dec4e7d1cadf032d66bcb032
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-11-24 07:32:09 +0330

    made some lil changes in codes of ripple button scripts

--- assets/plugins/ripple-button/ripple-Button.js
+++ assets/plugins/ripple-button/ripple-Button.js
@@ -2,20 +2,21 @@
 // Variables
 const allRippleButtons = document.querySelectorAll('.ripple-button');
 
-// Adding Event Listener Of Click On Mouse On Each Ripple Button That Creates Span Element.
+// A Function That That Creates Span Element.
 // Then Assigns Class Name To Created Span And Sets Top And Left Positions Of It Based On Clicked Position
 // After That Appends It To Clicked Button And Listens To AnimationEnd Event On Created Span Which Removes It
-allRippleButtons.forEach(item => {
-    item.addEventListener('mousedown', (event) => {
-        const innerSpan = document.createElement("span");
+function createRipple(itemToAppend) {
+    const innerSpan = document.createElement("span");
 
-        innerSpan.className = "ripple-button--inner-span";
-        innerSpan.style.left = `calc(${event.offsetX}px - 50%)`
-        innerSpan.style.top = `calc(${event.offsetY}px - 50%)`
-        innerSpan.style.width = `calc(${item.getBoundingClientRect().width}px - 0px)`
+    innerSpan.className = "ripple-button--inner-span";
+    innerSpan.style.left = `calc(${event.offsetX}px - 50%)`
+    innerSpan.style.top = `calc(${event.offsetY}px - 50%)`
+    innerSpan.style.width = `calc(${itemToAppend.getBoundingClientRect().width}px - 0px)`
 
-        item.appendChild(innerSpan);
+    itemToAppend.appendChild(innerSpan);
 
-        innerSpan.addEventListener('animationend', () => innerSpan.remove());
-    })
-})
+    innerSpan.addEventListener('animationend', () => innerSpan.remove());
+}
+
+// Adding Event Listener Of Click On Mouse On Each Ripple Button That Calls 'createRipple' Function
+allRippleButtons.forEach(item => {item.addEventListener('mousedown', (event) => {createRipple(item)})})
